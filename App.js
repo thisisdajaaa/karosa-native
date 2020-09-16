365da2638280c8e1fdda63efa02bb351450c1a58
Karlo <karlo@42race.com>
2020-09-16 12:01:10 +0800

    splash screen

--- App.js
+++ App.js
@@ -5,11 +5,11 @@ import { StyleSheet, Text, View , Image, SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <Image source={require('./assets/ic_mobile_market.png')} style={styles.imageStyle} /> 
-      {/* <Text>Open up App.js to startttt working on your app!</Text> */}
+      <View style={styles.line}/>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </View>
   );
 }
 
@@ -17,14 +17,24 @@ const styles = StyleSheet.create({
   container: {
     flex : 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignSelf: 'center',
+    width:'50%',
   },
 
-  imageStyle: {
-    flex : 1,
-    alignSelf: 'center',
-    height:'50px', 
-    width:'50%'
+  
+  line: {
+    height:'2px', 
+    width:'50%',
+    backgroundColor: '#73D673',
+    top: '5px'
   },
+  
+
+  imageStyle: {
+    height:'5%', 
+    width:'100%',
+    resizeMode: 'contain',
+  },
+
 });
