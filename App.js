3af428e4d9b6ba74654831417634d3ae3f5f6c60
Karlo <karlo@42race.com>
2020-09-30 16:05:38 +0800

    login UI

--- App.js
+++ App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React , { useState } from 'react';
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View, Image, SafeAreaView, Modal } from 'react-native';
-import SplashScreen from './screen/SplashScreen';
+import { StyleSheet, View, Button ,Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './screen/LoginScreen';
-import AuthNavigation from './navigation/AuthNavigation';
 import routes from './navigation/routes';
 import DashBoardNavigation from './navigation/DashBoardNavigation';
-
+import Modal from 'react-native-modal';
+import {Formik} from 'formik'
+import LoginScreen from './screen/LoginScreen';
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(true);
+
   return (
+    <>
       <NavigationContainer>
         <Stack.Navigator >
           <Stack.Screen name={routes.DASHBOARD}  options={{headerShown: false}}  component={DashBoardNavigation} />
         </Stack.Navigator>
       </NavigationContainer>
+      <Button title="Show modal" onPress={() => setModalVisible(true)} />
+      <Modal
+      isVisible={modalVisible} 
+      onSwipeComplete = {()=>{
+        setModalVisible(false)
+      }}
+      swipeDirection='down' 
+      style={styles.modalContainer}>
+          <LoginScreen/>
+      </Modal>
+      </>
   );
 }
 
 const styles = StyleSheet.create({
-
+    modalContainer :{  
+      justifyContent : "flex-end",
+      margin : 0    
+    }
 });
