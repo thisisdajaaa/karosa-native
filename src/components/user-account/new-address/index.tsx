63b06f3b1d5867c85f36db774429478091d77ef8
BLITZKRIEG\Chan <chrislastimosa34@gmail.com>
2020-10-19 16:23:37 +0800

    My address screen, New address header

--- src/components/user-account/new-address/index.tsx
+++ src/components/user-account/new-address/index.tsx
@@ -1,1 +1,21 @@
-export {}
+import React from "react";
+
+import { Props as HeaderProps } from "../../atoms/base-screen/types";
+import { Screen } from "../../atoms/base-screen";
+import { useNavigation } from "@react-navigation/native";
+
+const NewAddress: React.FC = () => {
+  const { goBack } = useNavigation();
+
+  const headerProps: HeaderProps = {
+    header: {
+      iconName: "arrow-back",
+      title: "New Address",
+      press: {
+        left: () => goBack(),
+      },
+    },
+  };
+  return <Screen {...headerProps}></Screen>;
+};
+export default NewAddress;
