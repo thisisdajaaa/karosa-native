d6505019fd7ac397a30e26a33be474ed9b65e3a0
Karlo <karlo@42race.com>
2020-10-26 15:25:03 +0800

    Ionic framework drop down integration

--- src/components/atoms/formik/form-input/index.tsx
+++ src/components/atoms/formik/form-input/index.tsx
@@ -9,9 +9,11 @@ import {
 import { ErrorMessage } from "../error-message";
 import { TextInput } from "../../input";
 
-import { Props } from "./types";
+import { IonSelectionProps, Props } from "./types";
 import { BaseText } from "../../base-text";
 import { formInputStyle } from "../form-input/style";
+import { IonItem, IonLabel, IonSelect, IonSelectOption } from '@ionic/react';
+import { backgroundColor, color } from "@shopify/restyle";
 
 export const FormInput: React.FC<Props> = React.memo(({ name, ...props }) => {
   const [, meta, helpers] = useField(name);
@@ -115,6 +117,38 @@ export const FormInput2: React.FC<Props> = React.memo(
             customStyles={formInputStyle.box}
             {...props}
           />
+        </View>
+      </React.Fragment>
+    );
+  }
+);
+
+export const FormInputSelection: React.FC<IonSelectionProps> = React.memo(
+  ({ name, value,data, placeholder, ...props }) => {
+    const [targetValue, setValue] = useState(value);
+    return (
+      <React.Fragment>
+        <View style={formInputStyle.container}>
+        <IonItem lines={"none"} style={{alignItems: 'center',alignContent: 'center', width : "100%",height : "100%", paddingTop: 10}} color="red">
+            <IonLabel style={{fontSize: 18, paddingLeft: 5, alignSelf: 'center',}}>{name}</IonLabel>
+            <IonSelect style={{alignSelf: 'center',}} value={targetValue} placeholder={placeholder} onIonChange={e => setValue(e.detail.value)}>
+            {data.map(data => (
+                <IonSelectOption key={data.id} value={data.value}>
+                  {data.value}
+                </IonSelectOption>
+              ))}         
+            </IonSelect>
+        </IonItem>
+          
+          {/* <BaseText style={formInputStyle.TextStyle}>{name}</BaseText>
+          <TextInput
+            style={formInputStyle.textinputstyle}
+            placeholder={placeholder}
+            value={value}
+            // onBlur={(ev) => handleBlur(ev)}
+            customStyles={formInputStyle.box}
+            {...props}
+          /> */}
         </View>
       </React.Fragment>
     );
--- src/components/atoms/formik/form-input/types.ts
+++ src/components/atoms/formik/form-input/types.ts
@@ -2,7 +2,20 @@ import { TextInputProps } from "react-native";
 
 import { Props as InputProps } from "../../input/types";
 
+
+type SelectionData = {
+  id: number;
+  value: String;
+};
+
 export type Props = {
   name: string;
 } & TextInputProps &
   InputProps;
+
+export type IonSelectionProps = {
+  name: string;
+  data: Array<SelectionData>;
+} & TextInputProps &
+  InputProps;
+  
