7e96975647fe703b4ebac8bc6731472be061435d
Jay Vincent Orio <vince4596@gmail.com>
2020-11-21 05:21:26 +0800

    :wrench: Added credentials option to axios to allow cookies in browser

--- src/config/axios/instance.ts
+++ src/config/axios/instance.ts
@@ -2,6 +2,8 @@ import axios, { AxiosInstance } from "axios";
 
 const baseAxios: AxiosInstance = axios.create({
   baseURL: "http://localhost:4040",
+  withCredentials: true
+
 });
 
 export { baseAxios };
