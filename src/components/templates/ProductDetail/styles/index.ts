32e8d0b35d29719cc3be6a782328b0436b5e3536
DESKTOP-D0S8LM3\Chan <chrislastimosa34@gmail.com>
2021-04-13 11:27:55 +0800

    partial commit ready for disection

---
+++ src/components/templates/ProductDetail/styles/index.ts
@@ -0,0 +1,12 @@
+/**
+ *
+ * @format
+ *
+ */
+
+import {StyleSheet} from 'react-native';
+
+const ProductDetailStyles = StyleSheet.create({
+});
+
+export default ProductDetailStyles;
---
+++ src/screens/ShopProductDetails/ShopProductDetails.tsx
@@ -0,0 +1,21 @@
+/**
+ *
+ * ShopProductDetails
+ * @format
+ *
+ */
+ 
+import React, {FC} from 'react';
+
+import ShopProductDetailsConfig from './config';
+import type {PropsType} from './types';
+import ShopProductDetailsStyles from './styles';
+
+
+const ShopProductDetails: FC<PropsType> = (props) => {
+  const {} = props;
+
+  return ;
+};
+
+export default ShopProductDetails;
