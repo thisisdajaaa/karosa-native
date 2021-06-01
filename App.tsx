0448b6d96dc44e4d342b6f9d5806a4fb49a89310
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2021-06-01 15:39:41 +0800

    refactor(app): rearranged imports

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { LogBox } from "react-native";
+import { useMount } from "@app/hooks";
+import { internetCheck } from "@app/utils";
 import LoadAssets from "@app/components/load-assets";
 import BaseNavigator from "@app/navigators/index";
 import configureStore from "@app/redux/store";
-import ToastProvider from "@app/components/molecules/Toast/context/ToastProvider";
-import { useMount } from "@app/hooks";
-import { internetCheck } from "@app/utils";
+import ToastProvider from "@app/molecules/Toast/context/ToastProvider";
 
 const fonts = {
   "SFProText-Bold": require("./src/assets/fonts/SF-Pro-Text-Bold.otf"),
