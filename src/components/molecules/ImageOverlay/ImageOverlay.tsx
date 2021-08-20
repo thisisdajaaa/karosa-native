5939c31e49b9cff1bd6e44cb2cdd70ff5bd4ad99
rgberou <brotark@gmail.com>
2021-08-20 23:29:52 +0800

    refactor(rebase): rebase
    Rebase

--- src/components/molecules/ImageOverlay/ImageOverlay.tsx
+++ src/components/molecules/ImageOverlay/ImageOverlay.tsx
@@ -20,6 +20,7 @@ const ImageOverlay: FC<PropsType> = (props) => {
     textContent,
     viewTextStyle,
     mainContainerStyle,
+<<<<<<< HEAD
     imageHeight,
     imageWidth,
   } = props;
@@ -43,6 +44,23 @@ const ImageOverlay: FC<PropsType> = (props) => {
         <Text
           text={textContent || ""}
           textStyle={[ImageOverlayStyles.textStyle, textStyle]}
+=======
+  } = props;
+
+  return (
+    <View
+      style={
+        [mainContainerStyle, { flex: 1 }] || ImageOverlayStyles.mainContainer
+      }>
+      <Image
+        source={source}
+        style={imageStyle || ImageOverlayStyles.imageStyle}
+      />
+      <View style={viewTextStyle || ImageOverlayStyles.textViewStyle}>
+        <Text
+          text={textContent || "Sample"}
+          textStyle={textStyle || ImageOverlayStyles.textStyle}
+>>>>>>> c416e9e... feat(added new variation in chevronlist and added new component imageoverlay): new Components
         />
       </View>
     </View>
--- src/components/organisms/ListChevron/types/index.ts
+++ src/components/organisms/ListChevron/types/index.ts
@@ -6,22 +6,21 @@
 
  import { StyleProp, TextStyle } from "react-native";
 
- export type PropsType = {
-   title: string;
-   variation?: number;
-   listColor?: string;
-   subtitle?: string;
-   info?: string;
-   chevronColor?: string;
-   avatar?: string;
-   hasBottomDivider?: boolean;
-   required?: boolean;
-   infoStyle?: StyleProp<TextStyle>;
-   iconGroup?: string;
-   iconName?: string;
-   iconWidth?: number;
-   iconHeight?: number;
-   onPress: () => void;
-   hasChevron?: boolean;
- };
- 
+export type PropsType = {
+  title: string;
+  variation?: number;
+  listColor?: string;
+  subtitle?: string;
+  info?: string;
+  chevronColor?: string;
+  avatar?: string;
+  hasBottomDivider?: boolean;
+  required?: boolean;
+  infoStyle?: StyleProp<TextStyle>;
+  iconGroup?: string;
+  iconName?: string;
+  iconWidth?: number;
+  iconHeight?: number;
+  onPress: () => void;
+  hasChevron?: boolean;
+};
