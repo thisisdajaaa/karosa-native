6dc164c7fa925622c568263541b079e353488c6d
rgberou <brotark@gmail.com>
2021-08-21 08:41:00 +0800

    refactor(rebase): rebase

--- src/components/molecules/ImageOverlay/styles/index.ts
+++ src/components/molecules/ImageOverlay/styles/index.ts
@@ -4,45 +4,22 @@
  *
  */
 
-<<<<<<< HEAD
-import { theme } from "@app/styles";
-=======
->>>>>>> c416e9e... feat(added new variation in chevronlist and added new component imageoverlay): new Components
-import { StyleSheet } from "react-native";
-
-const ImageOverlayStyles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-<<<<<<< HEAD
-=======
-  imageStyle: {
-    width: 200,
-    height: 200,
-  },
->>>>>>> c416e9e... feat(added new variation in chevronlist and added new component imageoverlay): new Components
-  textViewStyle: {
-    position: "absolute",
-    justifyContent: "center",
-    alignItems: "center",
-<<<<<<< HEAD
-    backgroundColor: theme.colors.black,
-    opacity: 0.8,
-  },
-  textStyle: {
-=======
-    backgroundColor: "black",
-    width: 200,
-    height: 200,
-    opacity: 0.8,
-  },
-  textStyle: {
-    alignItems: "center",
->>>>>>> c416e9e... feat(added new variation in chevronlist and added new component imageoverlay): new Components
-    color: "white",
-  },
-});
-
-export default ImageOverlayStyles;
+ import { theme } from "@app/styles";
+ import { StyleSheet } from "react-native";
+ 
+ const ImageOverlayStyles = StyleSheet.create({
+   mainContainer: {
+     flex: 1,
+   },
+   textViewStyle: {
+     position: "absolute",
+     justifyContent: "center",
+     alignItems: "center",
+     opacity: 0.8,
+   },
+   textStyle: {
+     color: theme.colors.white,
+   },
+ });
+ 
+ export default ImageOverlayStyles;
