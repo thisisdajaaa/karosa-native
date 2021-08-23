b87eabfaf028db6aaf75e34c79a11e4296fd786d
rgberou <brotark@gmail.com>
2021-08-23 21:03:55 +0800

    fix(imageoverlay): merge fix errors

--- src/components/organisms/ImageOverlayReviews/styles/index.ts
+++ src/components/organisms/ImageOverlayReviews/styles/index.ts
@@ -4,11 +4,20 @@
  *
  */
 
+import { theme } from "@app/styles";
 import { StyleSheet } from "react-native";
 
 const ImageOverlayReviewsStyles = StyleSheet.create({
   rowContainer: {
     flexDirection: "row",
+  },
+  horizontalContainer: {
+    backgroundColor: theme.colors.white,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    alignItems: "center",
   },
 });
 
