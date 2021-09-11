c0996f0a7435229c978a55348d9a9a0a1c523389
rgberou <brotark@gmail.com>
2021-09-11 12:00:25 +0800

    refactor(rebase): rebase

--- src/components/templates/Home/Home.tsx
+++ src/components/templates/Home/Home.tsx
@@ -9,7 +9,11 @@ import React, { FC } from "react";
 import Header from "@app/molecules/Header";
 
 import HomeStyles from "./styles";
+import routes from "@app/navigators/routes";
+import Button from "@app/atoms/Button";
+import { useNavigation } from "@react-navigation/native";
 
+<<<<<<< HEAD
 const HomeTemplate: FC = () => {
   return (
     <>
@@ -21,6 +25,20 @@ const HomeTemplate: FC = () => {
         }}
       />
     </>
+=======
+const HomeTemplate: FC<PropsType> = (props) => {
+  const { screenProps } = props;
+  const { navigate } = useNavigation();
+  return (
+    <Screen {...screenProps}>
+      <BaseText text={"HOME"} />
+
+      <Button
+        title="Testing only"
+        onPress={() => navigate(routes.SHOP_PRODUCT_DETAILS)}
+      />
+    </Screen>
+>>>>>>> 9e2ca53... feat(bug fix on footer): bug fix on footer
   );
 };
 
