be767fa5e4c3ecfcac80e33b6020b98dc3c265e1
rgberou <brotark@gmail.com>
2021-10-23 07:16:18 +0800

    fix(rebase): rebase

--- src/components/templates/AddressSearchTemplate/types/index.ts
+++ src/components/templates/AddressSearchTemplate/types/index.ts
@@ -4,7 +4,18 @@
  *
  */
 
+import { GooglePlacesAutocompleteProps } from "react-native-google-places-autocomplete";
+
+// export type GoogleAutoCompleteProps ={
+//   placeholder?: string;
+//   apiKey: string;
+//   fetchDetails?: boolean;
+//   GooglePlacesSearchQueryRank: string;
+
+// }
+
 export type PropsType = {
   latitude: number;
   longitude: number;
+  googleAutoCompleteProps?: GooglePlacesAutocompleteProps;
 };
